f0bb96f925bafb7e74e40e93f57e2474074a65be
Daniel Lee <daniel.lee@circleci.com>
2023-06-11 21:53:32 -0700

    add db connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn         string
+		maxIdleTime string
+	}
 }
 
 type application struct {
@@ -18,10 +29,44 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "environment", "development", "dev|prod")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("DB_DSN"), "db connection string")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "db max idle time")
 
 	app := &application{
 		config: cfg,
 	}
 
+	// connect to database
+	db, err := openDB(cfg)
+	if err != nil {
+		log.Fatalf("error connecting to database: %v", err)
+	}
+
+	defer db.Close()
+
 	app.serve()
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
