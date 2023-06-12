92bece13314d18648ea36e0229f8b5b935fd67eb
Daniel Lee <daniel.lee@circleci.com>
2023-06-11 21:53:37 -0700

    add new route

--- cmd/routes.go
+++ cmd/routes.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"github.com/dannylee1020/url-shortener/cmd/api"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
 )
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
-	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", api.HealthcheckHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.HealthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/url/shorten", app.ShortenURL)
 
 	return router
 }
