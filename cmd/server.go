6bb4eaeb9b129df554bb35d924ae85d2ab6fe935
Daniel Lee <daniel.lee@circleci.com>
2023-06-14 15:34:38 -0700

    add graceful shutdown

--- cmd/server.go
+++ cmd/server.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -15,13 +19,43 @@ func (app *application) serve() error {
 		IdleTimeout: time.Minute,
 	}
 
+	shutdownError := make(chan error)
+
+	// run separate goroutine to intercept signal for shutting down server
+	// and pass it to shutdownError channel
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		s := <-quit
+
+		log.Printf("Shutting down server, signal: %v", s.String())
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		log.Printf("Completing background tasks, addr: %v", srv.Addr)
+
+		// relay any error that might be caused during shutdown
+		// if successful no error will be relayed to the channel
+		shutdownError <- srv.Shutdown(ctx)
+	}()
+
 	log.Printf("Starting server at: %v", app.config.port)
 
 	err := srv.ListenAndServe()
-
 	if !errors.Is(err, http.ErrServerClosed) {
 		return err
 	}
+
+	// check if there was any error while shutting down the server
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Stopped server at port: %v", srv.Addr)
 
 	return nil
 }
