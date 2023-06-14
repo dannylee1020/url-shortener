674540e5d35742b3537cf917f6888c41b1271590
Daniel Lee <daniel.lee@circleci.com>
2023-06-14 15:34:07 -0700

    error handling

--- cmd/errors.go
+++ cmd/errors.go
@@ -15,11 +15,16 @@ func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, st
 	}
 }
 
-func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request) {
 	message := "The server encountered a problem and could not process your request"
 	app.errorResponse(w, r, 500, message)
 }
 
 func (app *application) invalidRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+}
+
+func (app *application) rateLimitExceededResponse(w http.ResponseWriter, r *http.Request) {
+	message := "Rate limit exceeded"
+	app.errorResponse(w, r, http.StatusTooManyRequests, message)
 }
