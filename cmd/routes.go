a19f0407fc884689925f88c71ce20ad908b08e0c
Daniel Lee <daniel.lee@circleci.com>
2023-06-13 21:31:03 -0700

    adding routes for redirect

--- cmd/routes.go
+++ cmd/routes.go
@@ -9,7 +9,8 @@ func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.HealthcheckHandler)
-	router.HandlerFunc(http.MethodPost, "/v1/url/shorten", app.ShortenURL)
+	router.HandlerFunc(http.MethodPost, "/v1/shorten", app.ShortenURL)
+	router.Handle(http.MethodGet, "/v1/redirect/:url", app.RedirectHandler)
 
 	return router
 }
