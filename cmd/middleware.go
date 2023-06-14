1dd689fefded00971c9b26d4e6d2ed32a2cab4fc
Daniel Lee <daniel.lee@circleci.com>
2023-06-14 16:01:44 -0700

    implement ip based rate limiting

--- cmd/middleware.go
+++ cmd/middleware.go
@@ -3,25 +3,44 @@ package main
 import (
 	"net/http"
 	"sync"
+	"time"
 
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 )
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	var mu sync.Mutex
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
 
-	limiter := rate.NewLimiter(2, 4)
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ip := realip.FromRequest(r)
+
 		if app.config.limiter {
 			mu.Lock()
 
-			if limiter.Allow() == false {
+			if _, found := clients[ip]; !found {
+				clients[ip] = &client{
+					limiter:  rate.NewLimiter(2, 4),
+					lastSeen: time.Now(),
+				}
+			}
+
+			if clients[ip].limiter.Allow() == false {
+				mu.Unlock()
 				app.rateLimitExceededResponse(w, r)
 				return
 			}
-		}
 
+			mu.Unlock()
+		}
 		next.ServeHTTP(w, r)
 	})
 }
